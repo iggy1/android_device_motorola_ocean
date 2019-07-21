4fad552b97e27b886e4778c6cb5b376b96adec15
iggy1 <31067463+iggy1@users.noreply.github.com>
2019-07-21 17:20:37 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,6 +14,6 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/motorola/channel/
+LOCAL_PATH := device/motorola/ocean/
 
-PRODUCT_MAKEFILES := $(LOCAL_PATH)/omni_channel.mk
+PRODUCT_MAKEFILES := $(LOCAL_PATH)/omni_ocean.mk
